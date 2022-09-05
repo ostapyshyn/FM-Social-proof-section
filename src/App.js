2d359acc7a4165ef4219ab3e40d1eeed64aca571
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-09-05 23:15:26 +0300

    Add basic styles to the ReviewCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import ReviewCard from './components/ReviewCard';
+import colton from './assets/img/image-colton.jpg';
 
 function App() {
-  return <ReviewCard />;
+  return (
+    <ReviewCard
+      name="Colton Smith"
+      img={colton}
+      review="“ We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! ”"
+    />
+  );
 }
 
 export default App;
--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function ReviewCard() {
-  return <div>ReviewCard</div>;
+import styles from './ReviewCard.module.scss';
+
+function ReviewCard({ img, name, review }) {
+  return (
+    <section className={styles.container}>
+      <div>
+        <div>
+          <img src={img} alt="user" />
+        </div>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.buyer}>Verified Buyer</p>
+      </div>
+      <p className={styles.review}>{review}</p>
+    </section>
+  );
 }
 
 export default ReviewCard;
